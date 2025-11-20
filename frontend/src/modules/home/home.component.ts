e59458745fc09e01c117fdc9e8b88d4d3f944673
Ken <kenoh17@uw.edu>
2025-11-19 21:36:54 -0700

    started proj

---
+++ frontend/src/modules/home/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-page',
+  standalone: true,
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+})
+export class HomeComponent {}
